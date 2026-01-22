5b43afa7c1acd3f16ba6af02fa7bfaacf91605fb
Andrew Barba <barba@hey.com>
2026-01-22 12:21:42 -0500

    change default out

--- src/cli/compile.ts
+++ src/cli/compile.ts
@@ -30,7 +30,7 @@ export async function compileCommand(
 ): Promise<void> {
 	// Derive name from spec if not provided
 	const name = options.name ?? (await deriveBinaryName(spec));
-	const outfile = options.outfile ?? `./dist/${name}`;
+	const outfile = options.outfile ?? `./.specli/${name}`;
 
 	const target = options.target
 		? (options.target as Bun.Build.Target)
